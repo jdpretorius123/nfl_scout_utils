1bd30066697c92951e53d074f0dd57ae363beed5
Anthony Qu <anthonyylq@gmail.com>
2023-04-24 21:32:22 -0400

    updated tests.

--- tests/test_rf.py
+++ tests/test_rf.py
@@ -33,6 +33,26 @@ def test_model() -> None:
     output = rf.get_feature_label(df, [3, 4, 7, 8], 2)
     model = rf.fit(output[0], output[1])
     assert model.n_estimators == 10, "Fitting not correct."
+    assert model.classes_ == ["OLB", "RB"], "Classes not correct."
+    out_params = {'bootstrap': True,
+                  'ccp_alpha': 0.0,
+                  'class_weight': None,
+                  'criterion': 'entropy',
+                  'max_depth': None,
+                  'max_features': 'sqrt',
+                  'max_leaf_nodes': None,
+                  'max_samples': None,
+                  'min_impurity_decrease': 0.0,
+                  'min_samples_leaf': 1,
+                  'min_samples_split': 2,
+                  'min_weight_fraction_leaf': 0.0,
+                  'n_estimators': 10,
+                  'n_jobs': None,
+                  'oob_score': False,
+                  'random_state': 7,
+                  'verbose': 0,
+                  'warm_start': False}
+    assert model.get_params() == out_params, "Params not correct."
 
 
 def test_predict() -> None:
